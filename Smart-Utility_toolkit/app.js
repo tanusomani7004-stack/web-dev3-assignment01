f72d86e6110592afe0f092dd9d5fccd0ace137cc
Tanu <tanusomani7004@gmail.com>
2026-09-16 20:06:41 +0530

    Add app.js to demonstrate custom module usage

---
+++ Smart-Utility_toolkit/app.js
@@ -0,0 +1,10 @@
+// app.js
+
+const isEven = require("./isEven");
+
+console.log("Custom Module Demo");
+
+console.log("10 is even:", isEven(10));
+console.log("7 is even:", isEven(7));
+console.log("24 is even:", isEven(24));
+console.log("15 is even:", isEven(15));
